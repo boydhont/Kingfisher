c07cc41da41c0413ef75a60c1f92794d7c2a8ca4
boydhont <boydhont@gmail.com>
2020-11-15 17:29:59 +0100

    Added pseudocode for document retrieval

---
+++ scripts/extensions/dynamic/extension-05.js
@@ -0,0 +1,12 @@
+//----------------------------------
+//Extension: Additional
+//Path: Adds a download 
+//----------------------------------
+
+//Get the current url
+//Save the complete website of the url to a file or variable
+//Combines all the javascript objects used
+//Modifies the javascript object
+//  Add the commands that have been drawn
+//  Add a frame
+//Saves the modified javascript object to the desktop
