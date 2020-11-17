9d0d480f33d38a05a3de7bf001b78d6c8bdf9074
boydhont <boydhont@gmail.com>
2020-11-17 23:00:45 +0100

    Finetuned the difference between local and http fetch request

--- scripts/extensions/dynamic/extension-05.js
+++ scripts/extensions/dynamic/extension-05.js
@@ -14,24 +14,49 @@
 //Saves the modified javascript object to the desktop
 
 
+const GetJavaScriptContent = function(){
+    const GetJavaScriptReferences = function(){
 
-const GetJavaScriptContent = async function(){
-    for(const element of document.getElementsByTagName("script")){
-        const url = element.src;
-        window.location.href = url;
+        const isUrlsHttpSource = (urls) => {
+            for(const url of urls) if(url.startsWith("http")) return true;
+            return false;
+        }
+
+        const isUrlsLocalSource = (urls) => {
+            for(const url of urls) if(url.startsWith("file://")) return true;
+            return false;
+        }
+
+        let urls = [];
+        for(const element of document.getElementsByTagName("script")) if(element.src.endsWith(".js")) urls.push(element.src);
+
+        //window.location.href = url;
+
         //let response = await fetch('scripts/manager/manager.js');
         //console.log(response.text()); //only works on http
         //TODO request the documents over url on the same domain
         //TODO add the strings together
         //TODO save the string as a javascript object
+        
+        return {
+            isHttpSource: isUrlsHttpSource(urls),
+            isLocalSource: isUrlsLocalSource(urls),
+            srcArray: urls
+        }
     }
+
+    const javaScriptReferences = GetJavaScriptReferences();
+    if(javaScriptReferences.srcArray.length <= 0) return null;
+    if(javaScriptReferences.isHttpSource) console.log("A http fetch request"); //TODO write the fetch request
+    if(javaScriptReferences.isLocalSource) File.open((content) => console.log(content)); //TODO fetch the data from 
+    return null;
 }
 
 Commands.snapshot = function(document){
     return new Command(
     document, "snapshot",
     () => true,
-    () => GetJavaScriptContent(),
+    () => console.log(GetJavaScriptContent()),
     () => null
     );
 }
